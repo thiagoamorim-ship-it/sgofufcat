1243798173b470aa11b243b70d6c3567c611231c
thiagoamorim-ship-it <thiagoamorim@unitins.br>
2026-09-25 10:13:18 -0300

    Refactor App component to use layout and dashboardfeat: ativa novo dashboard do SGO

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
-<AppLayout>
-   <Dashboard />
-</AppLayout>
+import AppLayout from './components/layout/AppLayout';
+import Dashboard from './pages/Dashboard';
+
+export default function App() {
+  return (
+    <AppLayout>
+      <Dashboard />
+    </AppLayout>
+  );
+}
