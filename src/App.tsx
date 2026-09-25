b1e6018bccb17ec4ea6a86056bd9cfe62ca8aaea
thiagoamorim-ship-it <thiagoamorim@unitins.br>
2026-09-25 11:34:00 -0300

    feat: conecta modulo de regularidade

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import AppLayout from './components/layout/AppLayout';
 import Dashboard from './pages/Dashboard';
 import NotasFiscais from './pages/NotasFiscais';
+import Regularidade from './pages/Regularidade';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/notas-fiscais" element={<NotasFiscais />} />
+          <Route path="/regularidade" element={<Regularidade />} />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
