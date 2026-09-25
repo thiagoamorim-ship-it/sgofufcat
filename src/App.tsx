42acafa8eeda287479d55287eda486b7ed459b62
thiagoamorim-ship-it <thiagoamorim@unitins.br>
2026-09-25 11:02:48 -0300

    feat: adiciona estrutura de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
 import AppLayout from './components/layout/AppLayout';
 import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
-    <AppLayout>
-      <Dashboard />
-    </AppLayout>
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+
+          {/* Rotas dos módulos serão adicionadas aqui */}
+
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
   );
 }
